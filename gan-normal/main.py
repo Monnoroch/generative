87417d46e0bb96f147506ade41de3df7f1761173
Monnoroch <monnoroch@gmail.com>
2017-10-13 00:13:28 +0000

    f

--- gan-normal/main.py
+++ gan-normal/main.py
@@ -19,9 +19,13 @@ class ModelParams(object):
         self.g_learning_rate = args.g_learning_rate
         self.batch_size = args.batch_size
         self.d_l2_reg = args.d_l2_reg
+        self.g_l2_reg = args.g_l2_reg
         self.dropout = args.dropout
         self.input_mean = args.input_mean
         self.input_stddev = args.input_stddev
+        self.nn_generator = args.nn_generator
+        self.generator_features = args.generator_features
+        self.discriminator_features = args.discriminator_features
 
 
 class GanNormalModel(object):
@@ -67,6 +71,8 @@ class GanNormalModel(object):
         # Optionally add L2 regularization to the discriminator.
         if hparams.d_l2_reg != 0.0:
             self.discriminator_loss += hparams.d_l2_reg * add_n([tf.nn.l2_loss(v) for v in discriminator_variables])
+        if hparams.g_l2_reg != 0.0:
+            self.generator_loss += hparams.g_l2_reg * add_n([tf.nn.l2_loss(v) for v in generator_variables])
 
         # Optimize losses with Adam optimizer.
         self.generator_train = tf.train.AdamOptimizer(hparams.g_learning_rate).minimize(
@@ -82,27 +88,28 @@ class GanNormalModel(object):
         tf.summary.scalar("G/cost", self.generator_loss)
         tf.summary.scalar("P_real_on_real/", self.average_probability_real)
         tf.summary.scalar("P_real_on_fake/", self.average_probability_fake)
-        for i in range(len(self.means)):
-            tf.summary.scalar("G/mean:%d" % i, self.means[i])
-        for i in range(len(self.means)):
-            tf.summary.scalar("G/stddev:%d" % i, self.stddevs[i])
+        if not hparams.nn_generator:
+            tf.summary.scalar("G/mean", self.mean)
+            tf.summary.scalar("G/stddev", self.stddev)
+        tf.summary.histogram("Real data", self.real_input)
+        tf.summary.histogram("Fake data", self.generated)
         self.summaries = tf.summary.merge_all()
 
     def discriminator(self, input, hparams):
         """
         Discriminator is a theee-layer fully connected NN.
         """
-        # First fully connected layer with 256 features and optional dropout.
+        # First fully connected layer with N features and optional dropout.
         input_size = 1
-        features = 256
+        features = hparams.discriminator_features
         weights = tf.get_variable("weights_1", initializer=tf.truncated_normal([input_size, features], stddev=0.1))
         biases = tf.get_variable("biases_1", initializer=tf.constant(0.1, shape=[features]))
         hidden_layer = tf.nn.relu(tf.matmul(input, weights) + biases)
         if hparams.dropout != 0.0:
             hidden_layer = tf.nn.dropout(hidden_layer, hparams.dropout)
 
-        # Second fully connected layer with 256 features and optional dropout.
-        features = 256
+        # Second fully connected layer with N features and optional dropout.
+        features = hparams.discriminator_features
         weights = tf.get_variable("weights_2", initializer=tf.truncated_normal([input_size, features], stddev=0.1))
         biases = tf.get_variable("biases_2", initializer=tf.constant(0.1, shape=[features]))
         hidden_layer = tf.nn.relu(tf.matmul(input, weights) + biases)
@@ -110,37 +117,51 @@ class GanNormalModel(object):
             hidden_layer = tf.nn.dropout(hidden_layer, hparams.dropout)
 
         # Final linear layer to compute the classifier's logits.
-        classes = 1
-        weights = tf.get_variable("weights_out", initializer=tf.truncated_normal([features, classes], stddev=0.1))
-        biases = tf.get_variable("biases_out", initializer=tf.constant(0.1, shape=[classes]))
+        output_size = 1
+        weights = tf.get_variable("weights_out", initializer=tf.truncated_normal([features, output_size], stddev=0.1))
+        biases = tf.get_variable("biases_out", initializer=tf.constant(0.1, shape=[output_size]))
         return tf.matmul(hidden_layer, weights) + biases
 
     def generator(self, input, hparams):
         """
         Generator is just a linear transformation of the input.
         """
-        self.means = []
-        self.stddevs = []
-        result = tf.constant(0.)
-        for i in range(len(hparams.input_mean)):
-            mean = tf.Variable(tf.constant(0. + i))
+        if not hparams.nn_generator:
+            self.mean = tf.Variable(tf.constant(0.))
             # Standard deviation has to be positive, so we make sure it is by computing the absolute value of the variable.
-            stddev = tf.sqrt(tf.Variable(tf.constant(1.)) ** 2)
-            result += tf.random_normal([hparams.batch_size, 1], 0., 1.) * stddev + mean
-            self.means.append(mean)
-            self.stddevs.append(stddev)
-        return result
+            self.stddev = tf.sqrt(tf.Variable(tf.constant(1.)) ** 2)
+            return input * self.stddev + self.mean
+
+        # First fully connected layer with N features.
+        input_size = 1
+        features = hparams.generator_features
+        weights = tf.get_variable("weights_1", initializer=tf.truncated_normal([input_size, features], stddev=0.1))
+        biases = tf.get_variable("biases_1", initializer=tf.constant(0.1, shape=[features]))
+        hidden_layer = tf.nn.relu(tf.matmul(input, weights) + biases)
+
+        # Second fully connected layer with N features.
+        features = hparams.generator_features
+        weights = tf.get_variable("weights_2", initializer=tf.truncated_normal([input_size, features], stddev=0.1))
+        biases = tf.get_variable("biases_2", initializer=tf.constant(0.1, shape=[features]))
+        hidden_layer = tf.nn.relu(tf.matmul(input, weights) + biases)
+
+        # Final linear layer to generate the example.
+        output_size = 1
+        weights = tf.get_variable("weights_out", initializer=tf.truncated_normal([features, output_size], stddev=0.1))
+        biases = tf.get_variable("biases_out", initializer=tf.constant(0.1, shape=[output_size]))
+        return tf.matmul(hidden_layer, weights) + biases
 
     def data_batch(self, hparams):
         """
         Input data are just samples from N(mean, stddev).
         """
         count = len(hparams.input_mean)
-        result = tf.constant(0.)
+        componens = []
         for i in range(count):
-            result += (1./count) * tf.random_normal(
-                [hparams.batch_size, 1], hparams.input_mean[i], hparams.input_stddev[i])
-        return result
+            componens.append(tf.contrib.distributions.Normal(loc=hparams.input_mean[i], scale=hparams.input_stddev[i]))
+        return tf.contrib.distributions.Mixture(
+          cat=tf.contrib.distributions.Categorical(probs=[1./count] * count),
+          components=componens).sample(sample_shape=[hparams.batch_size, 1])
 
     def generator_input(self, hparams):
         """
@@ -166,12 +187,18 @@ def main(args):
     parser.add_argument("--load_checkpoint", help="Continue training from a checkpoint")
     parser.add_argument("--batch_size", type=int, default=32, help="The size of the minibatch")
     parser.add_argument("--d_learning_rate", type=float, default=0.01, help="The discriminator learning rate")
-    parser.add_argument("--g_learning_rate", type=float, default=0.1, help="The generator learning rate")
+    parser.add_argument("--g_learning_rate", type=float, default=0.02, help="The generator learning rate")
     parser.add_argument("--d_l2_reg", type=float, default=0.0005, help="The discriminator L2 regularization parameter")
-    parser.add_argument("--input_mean", type=float, default=[], help="The mean of the input dataset", action='append')
-    parser.add_argument("--input_stddev", type=float, default=[], help="The standard deviation of the input dataset", action='append')
+    parser.add_argument("--g_l2_reg", type=float, default=0., help="The generator L2 regularization parameter")
+    parser.add_argument("--input_mean", type=float, default=[], help="The mean of the input dataset", action="append")
+    parser.add_argument("--input_stddev", type=float, default=[], help="The standard deviation of the input dataset", action="append")
     parser.add_argument("--max_steps", type=int, default=2000, help="The maximum number of steps to train training for")
     parser.add_argument("--dropout", type=float, default=0.5, help="The dropout rate to use in the descriminator")
+    parser.add_argument("--discriminator_steps", type=int, default=1, help="The number of steps to train the descriminator on each iteration")
+    parser.add_argument("--generator_steps", type=int, default=1, help="The number of steps to train the generator on each iteration")
+    parser.add_argument("--nn_generator", default=False, action="store_true", help="Whether to use a neural network as a generator")
+    parser.add_argument("--generator_features", default=256, type=int, help="The number of features in generators hidden layers")
+    parser.add_argument("--discriminator_features", default=256, type=int, help="The number of features in discriminators hidden layers")
     args = parser.parse_args(args)
     # Default input mean and stddev.
     if not args.input_mean:
@@ -193,26 +220,22 @@ def main(args):
         else:
             session.run(tf.global_variables_initializer())
 
-        summary_writer = tf.summary.FileWriter(args.summaries_dir + '/train', session.graph)
+        summary_writer = tf.summary.FileWriter(args.summaries_dir + "/train", session.graph)
 
         # The main training loop. On each interation we train both the discriminator and the generator on one minibatch.
         for step in range(args.max_steps):
             print_graph(session, model, step)
             # First, we run one step of discriminator training.
-            session.run(model.discriminator_train)
+            for _ in range(max(int(args.discriminator_steps/2), 1)):
+                session.run(model.discriminator_train)
             # Then we run one step of generator training.
-            session.run(model.generator_train)
+            for _ in range(args.generator_steps):
+                session.run(model.generator_train)
+            for _ in range(int(args.discriminator_steps/2)):
+                session.run(model.discriminator_train)
             # And export all summaries to tensorboard.
             summary_writer.add_summary(session.run(model.summaries), step)
             # saver.save(session, "%s/model.ckpt-%d" % (args.train_dir, step))
-
-        # The main training loop. On each interation we train both the discriminator and the generator on one minibatch.
-        for step in range(1000):
-            print_graph(session, model, args.max_steps + step)
-            # First, we run one step of discriminator training.
-            session.run(model.discriminator_train)
-            # And export all summaries to tensorboard.
-            summary_writer.add_summary(session.run(model.summaries), args.max_steps + step)
 
 
 if __name__ == "__main__":
