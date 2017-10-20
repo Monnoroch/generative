533f2073d37e29d9b643693675f381ac47cea6ef
Monnoroch <monnoroch@gmail.com>
2017-10-20 18:49:20 +0000

    f

--- linear_regression/test.sh
+++ linear_regression/test.sh
@@ -1,4 +1,4 @@
 set +e
 
 PYTHONPATH=. python linear_regression/train.py --experiment_dir test-exps/linear_regression --l2_reg 0.01 --learning_rate 0.01 --input_param1 0.8 --input_param2 1 --batch_size 10 --max_steps 2
-PYTHONPATH=. python linear_regression/train.py --experiment_dir test-exps/linear_regression --l2_reg 0.05 --learning_rate 0.05 --batch_size 10 --max_steps 2 --load_checkpoint test-exps/linear_regression/model/checkpoint-2/data
+PYTHONPATH=. python linear_regression/train.py --experiment_dir test-exps/linear_regression --l2_reg 0.05 --learning_rate 0.05 --batch_size 5 --max_steps 2 --load_checkpoint test-exps/linear_regression/model/checkpoint-2/data
