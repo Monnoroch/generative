bab6241661e17e309c6636671560f3402f3588fa
Max <monnoroch@gmail.com>
2017-10-30 11:19:17 +0000

    A deep NN GAN model. (#8)

---
+++ gan_deep_nn/test.sh
@@ -0,0 +1,16 @@
+echo "Running $0..."
+
+set +e
+
+STEPS=2
+EXPERIMENT=test-exps/gan-nn
+
+python gan_deep_nn/train.py --experiment_dir $EXPERIMENT --dataset_dir mnist-data \
+    --d_l2_reg 0.01 --discriminator_steps 1 --generator_steps 1 --d_learning_rate 0.01 --g_learning_rate 0.01 \
+    --g_l2_reg 0.01 --dropout 0.1 --generator_features 10 --generator_features 5 --discriminator_features 10 \
+    --discriminator_features 15 --batch_size 10 --max_steps $STEPS
+
+python gan_deep_nn/train.py --experiment_dir $EXPERIMENT --dataset_dir mnist-data \
+    --d_l2_reg 0.02 --discriminator_steps 2 --generator_steps 3 --d_learning_rate 0.03 --g_learning_rate 0.04 \
+    --g_l2_reg 0.05 --dropout 0.6 --batch_size 10 --max_steps 2 \
+    --load_checkpoint $EXPERIMENT/model/checkpoint-$STEPS/data
---
+++ gan_deep_nn/train.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+import sys
+
+import tensorflow as tf
+
+from gan_deep_nn import model
+from common.experiment import Experiment
+from datasets.mnist import mnist_dataset
+
+
+def print_graph(session, model, step):
+    """
+    A helper function for printing key training characteristics.
+    """
+    real, fake = session.run([model.average_probability_real, model.average_probability_fake])
+    print("Step %d; real = %f, fake = %f" % (step, real, fake))
+
+
+def make_dataset(args):
+    train_dataset = mnist_dataset(args.dataset_dir, train=True)
+    test_dataset = mnist_dataset(args.dataset_dir, train=False)
+    dataset = train_dataset.concatenate(test_dataset)
+    dataset = dataset.map(lambda image, label: image) # Only images.
+    dataset = dataset.map(lambda image: tf.reshape(image, [28, 28, 1]))
+    dataset = dataset.repeat()
+    dataset = dataset.batch(args.batch_size)
+    return dataset.make_one_shot_iterator()
+
+
+def main(args):
+    """
+    The main function to train the model.
+    """
+    parser = argparse.ArgumentParser(description="Train the gan-normal model.")
+    parser.add_argument("--dataset_dir", required=True, help="The path to the MNIST dataset files. If doesn't exist, the dataset will be downloaded")
+    parser.add_argument("--experiment_dir", required=True, help="The expriment directory to store all the data")
+    parser.add_argument("--load_checkpoint", help="Continue training from a checkpoint")
+    parser.add_argument("--batch_size", type=int, default=32, help="The size of the minibatch")
+    parser.add_argument("--d_learning_rate", type=float, default=0.01, help="The discriminator learning rate")
+    parser.add_argument("--g_learning_rate", type=float, default=0.02, help="The generator learning rate")
+    parser.add_argument("--d_l2_reg", type=float, default=0.0005, help="The discriminator L2 regularization parameter")
+    parser.add_argument("--g_l2_reg", type=float, default=0., help="The generator L2 regularization parameter")
+    parser.add_argument("--max_steps", type=int, default=2000, help="The maximum number of steps to train training for")
+    parser.add_argument("--dropout", type=float, default=0.5, help="The dropout rate to use in the descriminator")
+    parser.add_argument("--discriminator_steps", type=int, default=1, help="The number of steps to train the descriminator on each iteration")
+    parser.add_argument("--generator_steps", type=int, default=1, help="The number of steps to train the generator on each iteration")
+    parser.add_argument("--generator_features", default=[], action="append", type=int, help="The number of features in generators hidden layers")
+    parser.add_argument("--discriminator_features", default=[], action="append", type=int, help="The number of features in discriminators hidden layers")
+    parser.add_argument("--latent_space_size", default=256, type=int, help="The number of features in generator input")
+    args = parser.parse_args(args)
+
+    experiment = Experiment(args.experiment_dir)
+    hparams = experiment.load_hparams(model.ModelParams, args)
+
+    dataset = make_dataset(args)
+    model_ops = model.GanModel(hparams, dataset, model.TrainingParams(args, training=True))
+
+    stop_generator_op = (model_ops.average_probability_fake > model_ops.average_probability_real)
+
+    saver = tf.train.Saver()
+    with tf.Session() as session:
+        # Initializing the model. Either using a saved checkpoint or a ranrom initializer.
+        if args.load_checkpoint:
+            saver.restore(session, args.load_checkpoint)
+        else:
+            session.run(tf.global_variables_initializer())
+
+        summary_writer = tf.summary.FileWriter(experiment.summaries_dir(), session.graph)
+
+        # The main training loop. On each interation we train both the discriminator and the generator on one minibatch.
+        global_step = session.run(model_ops.global_step)
+        for _ in range(args.max_steps):
+            print_graph(session, model_ops, global_step)
+            # First, we run one step of discriminator training.
+            for _ in range(args.discriminator_steps):
+                session.run(model_ops.discriminator_train)
+
+            # Then we run one step of generator training.
+            for _ in range(args.generator_steps):
+                session.run(model_ops.generator_train)
+
+            # Increment global step.
+            session.run(model_ops.increment_global_step)
+            global_step = session.run(model_ops.global_step)
+            # And export all summaries to tensorboard.
+            summary_writer.add_summary(session.run(model_ops.summaries), global_step)
+
+        # Save experiment data.
+        saver.save(session, experiment.checkpoint(global_step))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
