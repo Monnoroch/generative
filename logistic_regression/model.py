f31539eb9a34491d6fd1c0c87bc43af98c4dd02d
Max <monnoroch@gmail.com>
2018-04-03 14:36:34 +0100

    Better graph structures for logistic and linear regressions. (#24)

--- logistic_regression/model.py
+++ logistic_regression/model.py
@@ -19,36 +19,52 @@ class LogisticRegressionModel(object):
     """
     def __init__(self, dataset, training_params):
         # Set up the global step.
-        self.global_step = tf.Variable(0, name="global_step", trainable=False)
-        self.increment_global_step = tf.assign_add(self.global_step, 1)
+        with tf.name_scope("global_step_tools"):
+            self.global_step = tf.Variable(0, name="global_step", trainable=False)
+            self.increment_global_step = tf.assign_add(self.global_step, 1)
 
-        # Compute the loss funstion -- cross-entropy between real and predicted labels.
-        labels, samples = dataset.get_next()
-        with tf.variable_scope("discriminator") as scope:
-            predicted_labels = self.discriminator(samples)
-            variables = [v for v in tf.global_variables() if v.name.startswith(scope.name)]
-            l2_reg_variables = [v for v in variables if v.name.find("weights") != -1]
+        self.variables = []
+        self.l2_reg_variables = []
 
-        self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            labels=tf.cast(labels, tf.float32), logits=predicted_labels))
-
-        # Add optional L2 regularization.
-        if training_params.l2_reg != 0.0:
-            self.loss += training_params.l2_reg * add_n([tf.nn.l2_loss(v) for v in l2_reg_variables])
+        labels, samples = self.input(dataset)
+        predicted_labels = self.discriminator(samples)
+        self.loss = self.get_loss(labels, predicted_labels, training_params)
 
         # Train the model with Adam.
-        self.train = tf.train.AdamOptimizer(training_params.learning_rate).minimize(
-            self.loss, var_list=variables, name="train")
+        with tf.name_scope("train"):
+            self.train = tf.train.AdamOptimizer(training_params.learning_rate).minimize(
+                self.loss, var_list=self.variables, name="train")
 
         # Export summaries.
-        tf.summary.scalar("Loss", self.loss)
-        self.summaries = tf.summary.merge_all()
+        with tf.name_scope("summaries"):
+            tf.summary.scalar("Loss", self.loss)
+            self.summaries = tf.summary.merge_all()
+
+    def input(self, dataset):
+        with tf.name_scope("input"):
+            return dataset.get_next()
 
     def discriminator(self, input):
         """
         The discriminator network to classify inputs. Returns logits.
         """
-        output_size = 1
-        self.param1 = tf.get_variable("weights", initializer=tf.truncated_normal([output_size], stddev=0.1))
-        self.param2 = tf.get_variable("biases", initializer=tf.constant(0.1, shape=[output_size]))
-        return input * self.param1 + self.param2
+        with tf.variable_scope("model"):
+            output_size = 1
+            weights = tf.get_variable("weights", initializer=tf.truncated_normal([output_size], stddev=0.1))
+            self.variables.append(weights)
+            self.l2_reg_variables.append(weights)
+            biases = tf.get_variable("biases", initializer=tf.constant(0.1, shape=[output_size]))
+            self.variables.append(biases)
+            return input * weights + biases
+
+    def get_loss(self, labels, predicted_labels, training_params):
+        # Compute the loss funstion -- cross-entropy between real and predicted labels.
+        with tf.name_scope("loss"):
+            loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=tf.cast(labels, tf.float32), logits=predicted_labels))
+
+            # Add optional L2 regularization.
+            if training_params.l2_reg != 0.0:
+                loss += training_params.l2_reg * add_n([tf.nn.l2_loss(v) for v in self.l2_reg_variables])
+
+            return loss
